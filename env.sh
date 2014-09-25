467d8e2b4001f5d0e42885c6d502844578d825f9
Tom Bamford <tom@bamford.co.za>
2014-09-25 10:11:50 +0200

    Better editor handling, coloured manpages, no cows for Ansible

--- env.sh
+++ env.sh
@@ -8,6 +8,24 @@ GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 export GIT_AUTHOR_NAME GIT_COMMITTER_NAME GIT_AUTHOR_EMAIL GIT_COMMITTER_EMAIL
 
-export EDITOR=vim
-
 alias vi=vim
+
+# Determine best editor
+for E in vim vi nano; do
+  if which $E 2>/dev/null 1>/dev/null; then
+    export EDITOR=$E
+    break
+  fi
+done
+
+# Colours for less and manpages
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+# Ansible cow-free zone
+export ANSIBLE_NOCOWS=1
